c5cb70204c55f62c9cd4c38165aa6be8a87d0b15
Khalil T laptop <tl.khalil@gmail.com>
2022-05-18 15:44:05 +0100

    work done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import './App.css'
+import photo from './photo.jpg'
 
-export default App;
+export default class App extends Component {
+  state={
+    Person :{
+      fullname:"Fat Freddy's Dropdown",
+      bio:"New Zealand seven-piece band from Wellington, whose musical style has been characterised as any combination of dub, reggae, soul, jazz, rhythm and blues, and techno.",
+      profession:"Artist",
+      imgSrc:photo
+    },
+    show:false,
+    count:0
+  }
+  componentDidMount() { 
+    setInterval(() => { 
+      this.setState({count:+(this.state.count) + 1})
+     }, 1000)
+   }
+  render() {
+    return (
+      <div className="App">
+        <h3>{this.state.count}</h3>
+        <button onClick={()=>this.setState({show:!this.state.show})}>Show</button>
+        {this.state.show?<div className='Person'>
+          <h1>{this.state.Person.fullname}</h1>
+          <p>{this.state.Person.bio}</p>
+          <h2>{this.state.Person.profession}</h2>
+        <img src={this.state.Person.imgSrc} alt="band cover"/>
+        </div>:null}
+      </div>
+    )
+     }
+
+  }
